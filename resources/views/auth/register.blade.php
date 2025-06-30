cf3a32b6c4715084998c10947d1ec369a9db108f
carloseduardorocha <carlos.rocha548@hotmail.com>
2025-06-29 21:51:39 -0300

    adjusts

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -41,12 +41,6 @@
 
         <div class="flex flex-col items-end mt-4 space-y-4">
             <div class="flex space-x-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                        {{ __('Esqueceu sua senha?') }}
-                    </a>
-                @endif
-
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                     {{ __('Já tem uma conta?') }}
                 </a>
